dc9089d30d5252be67c08703d7385fd4426dc484
Richard Ten <rten04@icloud.com>
2022-12-14 01:04:07 -0500

    Add SelectMode class
    Lets user pick between Coder and Breaker

---
+++ mode_select.rb
@@ -0,0 +1,27 @@
+require './display.rb'
+
+class SelectMode
+    include Display
+
+    def initialize
+        @success = false
+        logo
+        puts
+        introduction
+        puts instructions('mode')
+        until @success
+            @mode = gets.chomp.to_i
+            check_mode(@mode) 
+        end
+    end
+
+    def check_mode(mode)
+        if mode == 1 || mode == 2
+            @success = true
+            display_mode(mode)
+            return
+        else
+            puts error_messages('mode')
+        end
+    end
+end
