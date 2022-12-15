0bf98c5b8fec168696ce703f14bdfcca9f006b90
Richard Ten <rten04@icloud.com>
2022-12-15 08:30:16 -0500

    Add/modify methods

--- breaker.rb
+++ breaker.rb
@@ -5,10 +5,14 @@ class Breaker
 
     attr_accessor :guess, :attempts_left
 
+    @@valid_nums = ['1', '2', '3', '4', '5', '6']
+
     def initialize
         @finished = false
+        @got_guess = false
         @code = generate_code
-        @guess = []
+        @hints = ''
+        @guess = ''
         @attempts_left = 12
     end
 
@@ -25,19 +29,42 @@ class Breaker
         until @finished
             puts instructions('breaker_directions')
             puts display_attempts(@attempts_left)
-            while @guess.length < 4
-                @guess.push(get_guess)
+            until @got_guess
+                @guess = get_guess
+                valid_guess
             end
             check_guess(@guess)
-            @finished = true
+            puts display_code(@guess)
         end
     end
 
     def get_guess
+        guess = gets.chomp.to_s
+        return guess
+    end
 
+    def valid_guess
+        if @guess.length != 4
+            if @guess.length > 4
+                puts error_messages('less_nums')
+            else
+                puts error_messages('more_nums')
+            end
+        end
+
+        @guess = @guess.split('')
+        @guess.each do |char|
+            if @@valid_nums.include?(char)
+                next
+            else
+                puts error_messages('not_nums')
+                return
+            end
+        end
+            @got_guess = true
+            @finished = true
     end
 
     def check_guess
-
     end
 end
